0f7efe2c2e5b046b707656d854f60530ea73c124
echball <103051641+echball@users.noreply.github.com>
2022-11-16 12:02:09 -0500

    Update CheckSumTemplate.php

--- CheckSumTemplate.php
+++ CheckSumTemplate.php
@@ -1,11 +1,11 @@
 <?php
   $arr= array (363,294,793,914,644,895,258,492,164,514);
       
-function mod_ten($arr){
+function mod_hundred($arr){
   
 }
 
-function verify_mod_ten($checksum,$mod,$arr){
+function verify_mod_hundred($checksum,$mod,$arr){
 }
 
   ?>
